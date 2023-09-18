96469eb137ef26d055512968b2059bd66b29125c
Jackie Li <li.jackie565@gmail.com>
2023-09-18 00:28:18 -0400

    ui stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
+"use client";
 import Header from "@/components/Marginalia/Header";
 import HeaderText from "@/components/HeaderText";
 import Link from "next/link";
 export default function Page() {
   return (
     <main className="bg-elevated-base h-screen">
-      <Header
-        action={
-          <Link
-            className="text-lg text-sub-gray hover:text-primary-green transition-colors"
-            href={"/Login"}
-          >
-            Admin Login
-          </Link>
-        }
-      />
-
-      <HeaderText />
+      <section className="h-full flex flex-col justify-between">
+        <div>
+          <Header
+            action={
+              <Link
+                className="text-lg text-sub-gray hover:text-primary-green transition-colors"
+                href={"/Login"}
+              >
+                Admin Login
+              </Link>
+            }
+          />
+          <HeaderText />
+        </div>
+        <div className="flex justify-center w-full">
+          <div className="w-2/3 border border-primary-green h-[350px] rounded-t-xl"></div>
+        </div>
+      </section>
     </main>
   );
 }
