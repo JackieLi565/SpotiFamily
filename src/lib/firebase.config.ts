bb89d1b0c34e19e0257d31e389c883da3e95622b
Jackie Li <li.jackie565@gmail.com>
2023-09-23 17:00:59 -0400

    fix: firebase env

--- src/lib/firebase.config.ts
+++ src/lib/firebase.config.ts
@@ -6,7 +6,9 @@ const createAppInstance = () => {
   const cert = admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join(
+      "\n"
+    ),
   });
 
   return admin.initializeApp({
