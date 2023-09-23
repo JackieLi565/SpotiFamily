ad0ba13cf18925cace135c814960c7577c3e744a
Jackie Li <li.jackie565@gmail.com>
2023-09-23 16:52:45 -0400

    fix: callback

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,9 @@
 import SpotifyWebApi from "spotify-web-api-node";
 
-const webUrl = "http://localhost:3000";
-
 export const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_SECRET,
-  redirectUri: `${webUrl}/api/callback`,
+  redirectUri: `${process.env.WEB_URL}/api/callback`,
 });
 
 export const collections = {
